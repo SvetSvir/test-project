83955cd77baed343884d13195ada83237d05413c
svetl <svetlana.svir869@gmail.com>
2019-11-10 22:56:20 +0300

    Пример по теме "Массивы"

--- testobjects/src/main/java/Main.java
+++ testobjects/src/main/java/Main.java
@@ -1,7 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("args = " + Arrays.deepToString(args));
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String[] list = new String[10];
+            for (int i = 0; i < 8; i++)
+            {
+                list[i] = reader.readLine();
+
+            }
+            for (int j = list.length - 1; j >= 0; j--) {
+                System.out.println(list[j]);
+            }
+
     }
 }
